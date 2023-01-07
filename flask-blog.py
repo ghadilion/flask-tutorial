1d005907cc9d406959dac3a9ba8bf41f38d9bcfe
kiran <abdalhgn@gmail.com>
2023-01-07 21:04:41 +0530

    tutorial 2 final

--- flask-blog.py
+++ flask-blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
@@ -16,6 +16,7 @@ posts = [
     }
 ]
 
+@app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
