4ee4cbc13b41b939ede0ab151517a14ae4f9f95c
kiran <abdalhgn@gmail.com>
2023-01-07 11:01:18 +0530

    tutorial 2.2

--- flask-blog.py
+++ flask-blog.py
@@ -20,5 +20,10 @@ posts = [
 def home():
     return render_template('home.html', posts=posts)
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
