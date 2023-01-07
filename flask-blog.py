fa2ac3d27e49f0b60b6551aca1e2f2175794c660
kiran <abdalhgn@gmail.com>
2023-01-07 10:44:45 +0530

    tutorial 2.1

--- flask-blog.py
+++ flask-blog.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "<h1>hello world</h1>"
+posts = [
+    {
+        'author': 'Kiran Srinivasan',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': '01 January 2023'
+    },
+    {
+        'author': 'ABC',
+        'title': 'Blog post 1',
+        'content': 'Second post content',
+        'date_posted': '02 January 2023'
+    }
+]
+
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
